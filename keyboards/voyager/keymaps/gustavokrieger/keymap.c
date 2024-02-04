dce731cbc2923c01adcf0fa9a21aa7775769aa66
Gustavo Kriger <gustavopcassol@gmail.com>
2024-02-04 17:26:22 +0100

    wip

--- keyboards/voyager/keymaps/gustavokrieger/keymap.c
+++ keyboards/voyager/keymaps/gustavokrieger/keymap.c
@@ -2,17 +2,13 @@
 #include "version.h"
 #define MOON_LED_LEVEL LED_LEVEL
 
-enum custom_keycodes {
-    RGB_SLD = ML_SAFE_RANGE,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
   [0] = LAYOUT_voyager(
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_DELETE,      
     KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_ENTER,       
-    KC_TRANSPARENT, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       CW_TOGG,        
-    KC_TRANSPARENT, OSM(MOD_LGUI),  OSM(MOD_LALT),  OSM(MOD_LCTL),  OSM(MOD_LSFT),  KC_TRANSPARENT,                                 KC_TRANSPARENT, OSM(MOD_RSFT),  OSM(MOD_RCTL),  OSM(MOD_RALT),  OSM(MOD_RGUI),  KC_TRANSPARENT, 
+    KC_NO,          KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       CW_TOGG,        
+    KC_NO,          OSM(MOD_LGUI),  OSM(MOD_LALT),  OSM(MOD_LCTL),  OSM(MOD_LSFT),  KC_NO,                                          KC_NO,          OSM(MOD_RSFT),  OSM(MOD_RCTL),  OSM(MOD_RALT),  OSM(MOD_RGUI),  KC_NO,          
                                                     KC_SPACE,       TT(2),                                          TT(1),          KC_BSPC
   ),
   [1] = LAYOUT_voyager(
@@ -20,14 +16,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_0,           KC_4,           KC_5,           KC_6,           KC_PGDN,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_F12,         KC_NO,          KC_1,           KC_2,           KC_3,           KC_F5,                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_NO,          KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_NO,                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
-                                                    KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
+                                                    KC_TRANSPARENT, KC_TRANSPARENT,                                 TO(0),          KC_TRANSPARENT
   ),
   [2] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_NO,          KC_HOME,        KC_END,         KC_MINUS,       KC_EQUAL,       KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_QUOTE,       KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_F6,          KC_NO,          KC_LBRC,        KC_RBRC,        KC_BSLS,        KC_F11,         
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_NO,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_NO,          
-                                                    KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
+                                                    KC_TRANSPARENT, TO(0),                                          KC_TRANSPARENT, KC_TRANSPARENT
   ),
     // clang-format on
 };
@@ -41,9 +37,9 @@ void keyboard_post_init_user(void) {
 const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
     [0] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
 
-    [1] = {{0, 0, 0}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {0, 0, 0}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    [1] = {{0, 0, 0}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {0, 0, 0}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
 
-    [2] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {0, 0, 0}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {0, 0, 0}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {0, 0, 0}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {163, 86, 132}, {0, 0, 0}, {0, 0, 0}},
+    [2] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {0, 0, 0}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {0, 0, 0}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {0, 0, 0}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {69, 109, 126}, {0, 0, 0}, {0, 0, 0}},
 
 };
 
@@ -85,13 +81,29 @@ bool rgb_matrix_indicators_user(void) {
     return true;
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool aux(uint16_t keycode) {
     switch (keycode) {
-        case RGB_SLD:
-            if (record->event.pressed) {
-                rgblight_mode(1);
-            }
+        case KC_A ... KC_Z:
+        case KC_ENTER:
+        case KC_ESCAPE:
+        case KC_BACKSPACE:
+        case KC_SPACE:
+        case KC_TAB:
+        case KC_DELETE:
+        case KC_SEMICOLON:
+        case KC_COMMA:
+        case KC_DOT:
+        case KC_SLASH:
+        case QK_LAYER_TAP_TOGGLE ... QK_LAYER_TAP_TOGGLE_MAX:
+            return true;
+        default:
             return false;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed && aux(keycode)) {
+        layer_clear();
     }
     return true;
 }
