257bde353c4df0c77114a725b69a97022adde82b
Gustavo Kriger <gustavopcassol@gmail.com>
2026-02-16 00:31:15 -0300

    wip

--- keyboards/zsa/voyager/keymaps/bilateral/keymap.c
+++ keyboards/zsa/voyager/keymaps/bilateral/keymap.c
@@ -7,8 +7,8 @@
 
 enum custom_keycodes {
     RGB_SLD = ZSA_SAFE_RANGE,
-    LEFT_LAYER,
-    RIGHT_LAYER,
+    LEFT,
+    RIGHT,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_DELETE,
     KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_ENTER,
     KC_HYPR,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_HYPR,
-                                                    KC_SPACE,       LEFT_LAYER,                                          KC_BSPC,        RIGHT_LAYER
+                                                    KC_SPACE,       LEFT,                                          KC_BSPC,        RIGHT
   ),
   [1] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -152,7 +152,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
 
-        case LEFT_LAYER:
+        case LEFT:
             if (record->event.pressed) {
                 layer_move(1);
                 layer_on(2);
@@ -160,13 +160,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 if (stop) {
                     layer_off(1);
+                    layer_off(2);
                     stop = false;
+                } else if (get_mods() != 0) {
+                    layer_off(2);
                 }
-                layer_off(2);
                 layer = false;
             }
             return false;
-        case RIGHT_LAYER:
+        case RIGHT:
             if (record->event.pressed) {
                 layer_move(3);
                 layer_on(4);
@@ -174,9 +176,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 if (stop) {
                     layer_off(3);
+                    layer_off(4);
                     stop = false;
+                } else if (get_mods() != 0) {
+                    layer_off(4);
                 }
-                layer_off(4);
                 layer = false;
             }
             return false;
@@ -186,10 +190,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             // case KC_HYPR:
             break;
         default:
-            stop = true;
-            if (!record->event.pressed && !layer && get_mods() == 0) {
-                layer_move(0);
-                stop = false;
+            if (record->event.pressed && layer) {
+                stop = true;
             }
             break;
     }
@@ -201,9 +203,26 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_LCTL ... KC_RGUI:
         case KC_MEH:
         case KC_HYPR:
-            if (!record->event.pressed && !layer && get_mods() == 0) {
-                layer_move(0);
-                stop = false;
+            if (record->event.pressed) {
+                if (!layer) {
+                    layer_off(2);
+                    layer_off(4);
+                }
+            } else {
+                if (!layer && get_mods() == 0) {
+                    layer_off(1);
+                    layer_off(3);
+                }
+            }
+            break;
+        default:
+            if (!record->event.pressed && !layer) {
+                layer_off(2);
+                layer_off(4);
+                if (get_mods() == 0) {
+                    layer_off(1);
+                    layer_off(3);
+                }
             }
             break;
     }
